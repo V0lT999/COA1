6fd871b1cfe21def5d47808ac0c1f7a5e093dd4b
cagema.erm <cagema.erm@gmail.com>
2022-03-20 19:50:57 +0300

    Back-Second_Rachinskogo Добавил вывод рассчета q

--- Task2/SecondRachinskogo.py
+++ Task2/SecondRachinskogo.py
@@ -1,23 +1,23 @@
 def second_rachinskogo(n, p):
-    counter = 1
+
     b = p % 10
     if b == 1:
-        q = int((p * 9 + 1) / 10)
+        b = 9
     elif b == 9:
-        q = int((p + 1) / 10)
-    else:
-        q = int((p * b + 1) / 10)
+        b = 1
 
-    a = int(p / 10)
-    result = {'result': False}
+    q = int((p * b + 1) / 10)
 
+    result = {'result': False, '1': f"q = (p * b + 1) / 10 = ({p} * {b} + 1) / 10 = {q}"}
+
+    counter = 2
     current_number = n
     while n > p and counter < 100:
         k = n % 10
         m = int(n / 10)
 
         n = m + k * q
-        result[f'{counter}'] = f"{m} + {k}*{q} = {n}"
+        result[f'{counter}'] = f"m = {m} k = {k}, {m} + {k}*{q} = {n}"
         if n >= current_number:
             break
             # По условию - нужно использовать другой метод, если n не уменьшается
